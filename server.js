ecb903d9e94b42eb38d6bf61aa01e54cf83408d4
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-25 14:00:04 +1000

    complete server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,6 +24,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
     useFindAndModify: false
   }
 );
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
