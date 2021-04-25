dbbc4713ae9cb121fa2e9ff81e0f82db5ccd2c4b
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-25 12:16:17 +1000

    create folder List and index.js file

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar justify-content-start">
+        <Link
+          to="/"
+          className={ window.location.pathname === "/" ? "nav-link active" : "nav-link" }
+        >
+          <h1>Google Books</h1>
+        </Link>
+        <Link
+          to="/"
+          className={ window.location.pathname === "/" ? "nav-link active" : "nav-link" }
+        >
+          Search
+        </Link>
+        <Link
+          to="/saved"
+          className={ window.location.pathname === "/saved" ? "nav-link active" : "nav-link" }
+        >
+          Saved
+        </Link>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
